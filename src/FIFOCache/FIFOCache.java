b1474a64a2b02d171c8ea8540f86f29c0b891300
Hugo Iturrieta <hugo.sud.ste@gmail.com>
2024-10-26 15:48:38 -0300

    Agregar métodos de la interfaz

--- src/FIFOCache/FIFOCache.java
+++ src/FIFOCache/FIFOCache.java
@@ -1,5 +1,25 @@
 package FIFOCache;
 
-public class FIFOCache {
+import list.PositionList;
+
+public class FIFOCache<E> implements FIFOCacheI<E> {
+
+	@Override
+	public boolean accessPage(E pageNumber) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public PositionList<E> getPageFrameStatus() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public int getPageFaultCount() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
 
 }
