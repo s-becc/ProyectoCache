2486023dc67dc700e307426fe324c9fce38f74fb
s-becc <santiagobecchina@gmail.com>
2024-10-26 17:01:43 -0300

    agregar implementar FIFOCache

--- src/FIFOCache/FIFOCache.java
+++ src/FIFOCache/FIFOCache.java
@@ -1,25 +1,63 @@
 package FIFOCache;
 
-import list.PositionList;
+import list.*;
+import map.*;
 
-public class FIFOCache<E> implements FIFOCacheI<E> {
-
-	@Override
-	public boolean accessPage(E pageNumber) {
-		// TODO Auto-generated method stub
-		return false;
+public class FIFOCache implements FIFOCacheI<Integer> {
+	private PositionList<Integer> ListaPaginas;
+	private Map<Integer,Integer> MapPaginas;
+	private int capacity;
+	private int fallos;
+	
+	public FIFOCache(int capacity) {
+		ListaPaginas = new ListaDobleEnlazada<>();
+		MapPaginas = new MapeoHash<>();
+		this.capacity = capacity;
+		fallos = 0;
 	}
 
 	@Override
-	public PositionList<E> getPageFrameStatus() {
+	public boolean accessPage(Integer pageNumber) {
 		// TODO Auto-generated method stub
-		return null;
+		boolean esta = false;
+		try {
+		if (MapPaginas.get(pageNumber) != null)
+			esta = true;
+		else {
+		loadPage(pageNumber);
+		fallos++;
+		  }
+		}catch (InvalidKeyException e) { e.printStackTrace();}
+		return esta;
+	}
+
+	@Override
+	public PositionList<Integer> getPageFrameStatus() {
+		// TODO Auto-generated method stub
+		return ListaPaginas;
 	}
 
 	@Override
 	public int getPageFaultCount() {
 		// TODO Auto-generated method stub
-		return 0;
+		return fallos;
+	}
+	private void loadPage(int pageNumber) {
+		ListaPaginas.addLast(pageNumber);
+		try {
+			MapPaginas.put(pageNumber,pageNumber);
+		} catch (InvalidKeyException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		if (ListaPaginas.size() > capacity) 
+			try {
+				ListaPaginas.remove(ListaPaginas.first());
+				MapPaginas.remove(pageNumber);
+			} catch (InvalidPositionException | EmptyListException | InvalidKeyException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
 	}
 
 }
